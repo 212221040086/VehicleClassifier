b3e8d4ca09c94bef502f2873968478196b8c95ab
kulaganachi <kulaganachiravi13@gmail.com>
2023-08-10 14:09:57 +0530

    initial commit done

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car=new Car();
+        MotorCycle motorCycle=new MotorCycle();
+
+        car.start();
+
+        motorCycle.stop();
+
+    }
+}
+    interface Vehicle{
+        abstract void start();
+        abstract void stop();
+
+    }
+    class Car implements Vehicle{
+        @Override
+        public void start() {
+            System.out.println("Car Started");
+            System.out.println();
+        }
+
+        @Override
+        public void stop() {
+            System.out.println("Car Stopped");
+            System.out.println();
+        }
+    }
+    class MotorCycle implements Vehicle{
+        @Override
+        public void start() {
+            System.out.println("Motorcycle Started");
+            System.out.println();
+        }
+
+        @Override
+        public void stop() {
+            System.out.println("Motorcycle Stopped");
+            System.out.println();
+        }
+    }
+
